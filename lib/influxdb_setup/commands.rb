99905b60e0564875b5ce6e29b7d425e960f3bf12
Donald Plummer <dplummer@avvo.com>
2015-09-28 12:45:40 -0700

    Handle influxdb connection errors

--- lib/influxdb_setup/commands.rb
+++ lib/influxdb_setup/commands.rb
@@ -2,27 +2,39 @@ module InfluxdbSetup
   class Commands
     attr_reader :config
 
+    def self.handle_errors(method_name)
+      return warn "[InfluxdbSetup] requires Ruby 2.2 or greater" if method_name.nil?
+      alias_method "original_#{method_name}", method_name
+      define_method method_name do |*args|
+        begin
+          send("original_#{method_name}", *args)
+        rescue InfluxDB::ConnectionError => e
+          puts "[InfluxdbSetup##{method_name}] Skipping... #{e.message}"
+        end
+      end
+    end
+
     def initialize
       @config = Config.new
     end
 
-    def create_db
+    handle_errors def create_db
       CreateDb.new(config).call if @config.enabled?
     end
 
-    def setup_shard_spaces
+    handle_errors def setup_shard_spaces
       SetupShardSpaces.new(config).call if @config.enabled?
     end
 
-    def create_user
+    handle_errors def create_user
       CreateUser.new(config).call if @config.enabled?
     end
 
-    def load_queries
+    handle_errors def load_queries
       LoadQueries.new(config).call if @config.enabled?
     end
 
-    def mark_deploy(commit)
+    handle_errors def mark_deploy(commit)
       MarkDeploy.new(config).call(commit) if @config.enabled?
     end
   end
