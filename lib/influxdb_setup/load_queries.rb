e5ca6e158b6a02943796f15c4f76e0a343e36e89
Matt <mpurdy@avvo.com>
2016-02-15 14:30:59 -0800

    Use inderscores for naming continuous queries

--- lib/influxdb_setup/load_queries.rb
+++ lib/influxdb_setup/load_queries.rb
@@ -11,7 +11,7 @@ module InfluxdbSetup
       expected_queries.each_with_index do |query, index|
         unless existing_queries.map(&:query).include?(query)
           log "Adding '#{query}'"
-          root.create_continuous_query("config query #{index}", db, query)
+          root.create_continuous_query("config_query_#{index}", db, query)
         else
           no_change += 1
         end
@@ -21,7 +21,7 @@ module InfluxdbSetup
 
       existing_queries.each do |query|
         unless expected_queries.include?(query.query)
-          log "Removing '#{query.name}'"
+          log "Removing '#{query.name}':'#{query.query}'"
           root.delete_continuous_query(query.name, db)
         end
       end
