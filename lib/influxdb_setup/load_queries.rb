1fbd83c903d12782dcdc7583ccc9502001604d22
Matt <mpurdy@avvo.com>
2016-02-15 15:42:27 -0800

    Fix continuous query logic
    Correctly access hash properties

--- lib/influxdb_setup/load_queries.rb
+++ lib/influxdb_setup/load_queries.rb
@@ -7,9 +7,17 @@ module InfluxdbSetup
       existing_queries = root.list_continuous_queries(db)
       expected_queries = YAML.load_file("db/influxdb_queries.yml")
 
+      # Delete first in case an old name is getting overwritten
+      existing_queries.each do |query|
+        unless expected_queries.include?(query["query"])
+          log "Removing '#{query['name']}':'#{query['query']}'"
+          root.delete_continuous_query(query["name"], db)
+        end
+      end
+
       no_change = 0
       expected_queries.each_with_index do |query, index|
-        unless existing_queries.map(&:query).include?(query)
+        unless existing_queries.map{|hash| hash["query"]}.include?(query)
           log "Adding '#{query}'"
           root.create_continuous_query("config_query_#{index}", db, query)
         else
@@ -18,13 +26,6 @@ module InfluxdbSetup
       end
 
       log "There were #{no_change} continuous queries that required no updates"
-
-      existing_queries.each do |query|
-        unless expected_queries.include?(query.query)
-          log "Removing '#{query.name}':'#{query.query}'"
-          root.delete_continuous_query(query.name, db)
-        end
-      end
     end
   end
 end
